87a21ffe2ad58d02796958757563567fb6330f14
njp <nicolasjavierp@gmail.com.ar>
2019-09-30 14:43:38 -0300

    Integrating api and db

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const dbDebugger = require('debug')('app:db');
 const config = require('config');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-//const Joi = require('joi');
-const first_middleware = require('./first_middleware');
+//const first_middleware = require('./first_middleware');
 const express = require('express');
 const morgan = require('morgan')
 const moment = require('moment-timezone');
@@ -20,7 +19,7 @@ generalDebugger('App Name: ' + config.get('name'));
 //generalDebugger('DB user: ' + config.get('db.user'));
 //generalDebugger('DB password: ' + config.get('db.password'));
 
-mongoose.connect(config.get('db.location'))
+mongoose.connect(config.get('db.location'),{ useNewUrlParser: true })
     .then(() => dbDebugger("Connected to MLAB db ..."))
     .catch(err => dbDebugger("Could not connect to MLAB db: "+err.message));
 
@@ -63,30 +62,86 @@ app.use(morgan(
     }
 ));
 // Last MiddleWare is always the route handler
-/*
-const lfgs = [
-    {id: 1, creator_id: '1', creator_btag:'Javu#2632', activity: 'Calus', date: ' ',time: ' ', time_zone: ' '},
-    {id: 2, creator_id: '1', creator_btag:'Javu#2632', activity: 'Last Wish', date: ' ',time: ' ', time_zone: ' '},
-    {id: 3, creator_id: '1', creator_btag:'Javu#2632', activity: 'Corona del Dolor', date: ' ',time: ' ', time_zone: ' '}
-]
-*/
-const lfgs_Schema = new mongoose.Schema({
-    name: String,
-    creator_id: String,
-    creator_btag: String,
-    activity: String,
-    //date_time: {type: Date("<YYYY-mm-ddTHH:MM:ssZ>"), default:Date("<YYYY-mm-ddTHH:MM:ssZ>").now},
-    date_time: {type: Date, default:Date.now},
-    time_zone: String
-});
 
-const LFG = mongoose.model('LFGS',lfgs_Schema);
-const lfg = new LFG({
-    creator_id: "376055309657047040",
-    creator_btag: "Javu#2632",
-    activity: "Ocaso",
-    time_zone: "America/BsAs"
-});
+
+async function createLFG(){
+    const lfg = new LFG({
+        creator_id: "376055309657047040",
+        creator_btag: "Javu#2632",
+        activity: "Ocaso",
+        time_zone: "America/BsAs",
+        participants: [ "Javu#2632" ]
+    });
+    
+    try{
+        const result = await lfg.save();
+        dbDebugger(result);
+    }
+    catch(ex){
+        for(field in ex.errors)
+            dbDebugger(ex.errors[field].message);
+    }
+    //const result = await lfg.save();
+    //dbDebugger(result);
+}
+
+//createLFG();
+
+async function getAllLFGS(){
+    return await LFG.find().select('activity participants');
+    //const lfgs = await LFG.find();
+    /*
+    //Custom filters!!!
+    .find({creator_btag: "Javu#2632"})
+.or ( [{item1: "test1"}, {item2: "test2"}, {name: /.*Javu$/}] )
+    .and ( [{item1: "test1"}, {item2: "test2"}] )
+    .limit(10)
+    .sort({creator_id: 1}) // 1 ascending // -1 descending o "creator_id" // "-creator_id"
+    .select({activity:1, date_time:1}); // Only display relevant data
+    */
+    //dbDebugger("List of LFG's:");
+    //dbDebugger(lfgs);
+}
+
+async function updateLFG(id){
+    //Query First
+    //find by id
+    const lfg = await LFG.findById(id);
+    if (!lfg) return;
+    //Modify
+    lfg.set({
+        activity : 'Calus'
+    });
+    //Save
+    const result = await lfg.save();
+    dbDebugger(result);
+}
+
+async function updateDirectlyLFG(id){
+    const result = await LFG.update( {_id: id},{
+        $set: {
+            activity: 'Argos'
+        }
+    });
+    dbDebugger(result);
+}
+
+
+async function removeLFG(id){
+    const result = await LFG.deleteOne( {_id: id} );
+    dbDebugger(result);
+}
+
+/*
+async function run() {
+    const lfg = await getAllLFGS();
+    dbDebugger("List of LFG's:");
+    dbDebugger(lfg);
+    updateLFG("5d8e2d20c88950313ce765f2");
+}
+
+run();
+*/
 
 app.get('/', (req, res) => {
     res.send('Revenants LFG APP !!');
--- lfgs/lfgs.js
+++ lfgs/lfgs.js
@@ -1,47 +1,90 @@
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
+const mongoose = require('mongoose');
+const generalDebugger = require('debug')('app:general');
+
 
 //DB SCHEMA !!!
-const lfgs = [
-    {id: 1, creator_id: '1', creator_btag:'Javu#2632', activity: 'Calus', date: ' ',time: ' ', time_zone: ' '},
-    {id: 2, creator_id: '1', creator_btag:'Javu#2632', activity: 'Last Wish', date: ' ',time: ' ', time_zone: ' '},
-    {id: 3, creator_id: '1', creator_btag:'Javu#2632', activity: 'Corona del Dolor', date: ' ',time: ' ', time_zone: ' '}
-]
+const lfgs_Schema = new mongoose.Schema({
+    creator_id: {type: String, required: true},// Coding exclusive to Mongoose
+    creator_btag: {type: String, required: true},
+    activity: {type: String, required: true},
+    /*
+    activity: {
+        type: String,
+        required: true,
+        enum: ['Ocaso', 'Asalto', 'Mision']
+    },
+    */
+    //date_time: {type: Date("<YYYY-mm-ddTHH:MM:ssZ>"), default:Date("<YYYY-mm-ddTHH:MM:ssZ>").now},
+    date_time: {type: Date, default:Date.now},
+    time_zone: String,
+    participants: [ String ]
+    /*
+    isValid: Boolean,
+    publish: {
+        type: String,
+        required: function(){return this.isValid} // wil be published if lfg isValid
+    }
+    */
+});
 
-router.get('/', (req, res) => {
+const LFG = mongoose.model('LFGS',lfgs_Schema);
+
+router.get('/', async (req, res) => {
+    const lfgs = await LFG.find().sort('date_time');
     res.send(lfgs);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const lfg = await LFG.findById(req.params.id);
+    if (!lfg) return res.status(404).send('LFG not found');
+    res.send(lfg);
+    /*
     const lfg = lfgs.find(l => l.id === parseInt(req.params.id)); 
     // We can also use let (define a veriable to reset later) or var (same as let)
     //if not found return 404
     if (!lfg) return res.status(404).send('LFG not found');
     res.send(lfg);
-    
+    */
 });
 
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
     const { error } = validateLFG(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    
-    const lfg = {
-        id: lfgs.length +1,
+    let lfg = new LFG({
         creator_id: req.body.creator_id,
         creator_btag: req.body.creator_btag,
         activity: req.body.activity,
         date: req.body.date,
         time: req.body.time,
-        time_zone: req.body.time_zone
-    };
+        time_zone: req.body.time_zone,
+        participants: req.body.participants
+    });
+    lfg = await lfg.save();
 
-    lfgs.push(lfg);
     res.send(lfg);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    const { error } = validateLFG(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const lfg = await LFG.findByIdAndUpdate(req.params.id, {
+        creator_id : req.body.creator_id,
+        creator_btag : req.body.creator_btag,
+        activity : req.body.activity,
+        date : req.body.date,
+        time : req.body.time,
+        time_zone : req.body.time_zone,
+        participants: req.body.participants
+    }, {new: true})
+
+    if (!lfg) return res.status(404).send('LFG not found');
+    res.send(lfg);
+
+    /*
     //Lookup LFG with id
     //Not found 404
     const lfg = lfgs.find(l => l.id === parseInt(req.params.id)); 
@@ -65,9 +108,14 @@ router.put('/:id', (req, res) => {
     lfg.time = req.body.time;
     lfg.time_zone = req.body.time_zone;
     res.send(lfg);
+    */
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    const lfg = await LFG.findByIdAndRemove(req.params.id);
+    if (!lfg) return res.status(404).send('LFG not found');
+    res.send(lfg);
+    /*
     //Find lfg
     //Not Exists 404
     const lfg = lfgs.find(l => l.id === parseInt(req.params.id)); 
@@ -79,6 +127,7 @@ router.delete('/:id', (req, res) => {
 
     //Return the lfg
     res.send(lfg);
+    */
 });
 
 function validateLFG(lfg){
@@ -88,7 +137,8 @@ function validateLFG(lfg){
         activity: Joi.string().required(),
         date: Joi.date().min('1-10-2019').iso(),
         time: Joi.string().min(3),
-        time_zone: Joi.string().min(3)
+        time_zone: Joi.string().min(3),
+        participants: Joi.array().items(Joi.string())
     };
     return Joi.validate(lfg, schema);
 };
